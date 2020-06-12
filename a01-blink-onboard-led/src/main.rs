e3d946852dc4e1faa37f3e3d2c30d24314de8cda
0x6f736f646f <blackd0t@protonmail.com>
2020-06-12 23:11:32 +0300

    Adding main

--- a01-blink-onboard-led/src/main.rs
+++ a01-blink-onboard-led/src/main.rs
@@ -19,6 +19,52 @@ use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
 // use cortex_m_semihosting::hprintln;
 use cortex_m::asm;
 use stm32f1xx_hal::serial::{Config, Serial};
+#[entry]
+fn main() -> ! {
+    // hprintln!("Hello, world!").unwrap();
+
+    // Get access to the core peripherals from the cortex-m crate
+    let cp = cortex_m::Peripherals::take().unwrap();
+    // Get access to the device specific peripherals from the peripheral access crate
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
+    // HAL structs
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
+    // `clocks`
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    // Acquire the GPIOC peripheral
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+
+    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
+    // in order to configure the port. For pins 0-7, crl should be passed instead.
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    // Configure the syst timer to trigger an update every second
+    let mut timer = Timer::syst(cp.SYST, &clocks).start_count_down(1.hz());
+
+    // Wait for the timer to trigger an update and change the state of the LED
+
+    // Prepare the alternate function I/O registers
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    // Prepare the GPIOB peripheral
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    // USART1
+    // let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+    // let rx = gpioa.pa10;
+
+    // USART1
+    // let tx = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
+    // let rx = gpiob.pb7;
+
+    // USART2
+    // let tx = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
+    // let rx = gpioa.pa3;
 
 fn main() {
     println!("Hello, world!");
