612e56c82b01ba332da5a8b9026d1cd65f88010a
0x6f736f646f <blackd0t@protonmail.com>
2020-06-12 23:10:59 +0300

    Adding imports

--- a01-blink-onboard-led/src/main.rs
+++ a01-blink-onboard-led/src/main.rs
@@ -5,6 +5,20 @@
 //! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
 //! the reference manual for an explanation. This is not an issue on the blue pill.
 
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+use nb::block;
+
+use cortex_m_rt::entry;
+use embedded_hal::digital::v2::OutputPin;
+use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+// use cortex_m_semihosting::hprintln;
+use cortex_m::asm;
+use stm32f1xx_hal::serial::{Config, Serial};
 
 fn main() {
     println!("Hello, world!");
